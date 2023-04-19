1bfeedfee6971729534e1fa25906d3c7b924887c
TRC-Abraham <128326307+TRC-Abraham@users.noreply.github.com>
2023-04-19 16:49:49 -0600

    Continuación de Etch-A-Sketch

--- Full-Stack-JavaScript/HTML-CSS-Intermedio/JavaScript-Basics/Ejercicios/Proyecto: Etch-A-Sketch/scripts.js
+++ Full-Stack-JavaScript/HTML-CSS-Intermedio/JavaScript-Basics/Ejercicios/Proyecto: Etch-A-Sketch/scripts.js
@@ -6,22 +6,26 @@ let currentColor = DEFAULT_COLOR;
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
 
-function setCurrentColor(newColor) {
-  currentColor = newColor
-}
-
-function setCurrentMode(newMode) {
-  activateButton(newMode)
-  currentMode = newMode
-}
-
-function setCurrentSize(newSize) {
-  currentSize = newSize
-}
-
 const grid = document.getElementById('drawArea')
 
 let btnSize = document.getElementById( 'btnSize' );
+
+//------------------------------------------------------
+
+function clearGrid() {
+	grid.innerHTML = '';
+}
+
+function reloadGrid() {
+	clearGrid();
+	setupGrid(currentSize);
+}
+
+function changeSize(value) {
+	let newSize = Number(prompt( 'Escribe un valor para la cuadricula: ', 64 ));
+	currentSize = newSize;
+	reloadGrid();
+}
 
 function setupGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -30,17 +34,31 @@ function setupGrid(size) {
     for (let i = 0; i < size * size; i++) {
         let celda = document.createElement('div')
         celda.classList.add('celda')
-        celda.addEventListener('mouseover', changeColor)
-        celda.addEventListener('mousedown', changeColor)
+        celda.addEventListener('mouseover', painting)
+        celda.addEventListener('mousedown', painting)
         grid.appendChild(celda)
     }
 }
+
+// Estas funciones periten el dibujado y cambio de tamaño y numero de celdas del area de pintado.
+
+//-----------------------------------------------------
+
+btnSize.addEventListener( 'click', changeSize );
+
+// Esto perite iniciar el cambio de celdas del area de pintado.
+
+//------------------------------------------------------
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-function changeColor(e) {
+//Eto permite que el programa sepa, que mientras se mantenga pulsado el click los divs del grid cambiaran de color (se pintaran). Y que al soltar el click se deje de pintar.
+
+//-----------------------------------------------------
+
+function painting(e) {
     if (e.type === 'mouseover' && !mouseDown) return
     if (currentMode === 'color') {
         e.target.style.backgroundColor = currentColor
@@ -49,6 +67,15 @@ function changeColor(e) {
     }
 }
 
+// Esta es la función encargada de pintar
+
+//------------------------------------------------------
+
 window.onload = () => {
     setupGrid(DEFAULT_SIZE)
 }
+
+// Esto permite iniciar el programa con un valor predeterminado 
+// para el area dee dibujo.
+
+//-----------------------------------------------------
